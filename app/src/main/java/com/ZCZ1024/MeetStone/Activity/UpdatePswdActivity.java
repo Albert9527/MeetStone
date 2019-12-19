a5b0fa9ff9bc56b959359585939a2b3a47ef5434
Albert9527 <55827613+Albert9527@users.noreply.github.com>
2019-12-19 17:59:36 +0800

    界面优化和部分模块的逻辑处理，增加修改密码页面。我的队伍界面新增弹出菜单

---
+++ app/src/main/java/com/ZCZ1024/MeetStone/Activity/UpdatePswdActivity.java
@@ -0,0 +1,16 @@
+package com.ZCZ1024.MeetStone.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ZCZ1024.MeetStone.R;
+
+public class UpdatePswdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_pswd);
+    }
+}
